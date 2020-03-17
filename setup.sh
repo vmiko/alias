0f61cb9e5eb4e55f91381502ca67399ff0bfb85b
Victor Mikolajczyk <victor.mikolajczyk@capgemini.com>
2020-03-17 21:36:50 +0100

    feat: ajoute un script shell setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+# Init .bashrc.d directory
+echo "Init .bashrc.d directory in your home directory"
+mkdir -p ~/.bashrc.d
+chmod 770 ~/.bashrc.d
+
+echo "Add .bashrc.d directory at the end of your .bashrc file if not exists"
+if [ -z "$(cat ~/.bashrc | grep '.bashrc.d')" ]; then
+  cat >> ~/.bashrc <<EOL
+
+  # Load all bashrc files in .bashrc.d directory 
+  # instead of write aliases in .bashrc file.
+  for file in ~/.bashrc.d/*.bashrc;
+  do
+    source "\$file"
+  done
+EOL
+fi
+
+echo "Add a bashrc alias to reload bashrc files"
+echo "alias bashrc='chmod +x ~/.bashrc.d/*.bashrc; source ~/.bashrc'" > ~/.bashrc.d/bashrc.bashrc
+echo "alias brc='bashrc'" >> ~/.bashrc.d/bashrc.bashrc
+
+echo "Grant permissions to all .babashrc files in .bashrc.d directory"
+chmod +x ~/.bashrc.d/*.bashrc
+
+echo "Done !"
